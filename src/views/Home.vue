4ccfa41c1b73252b5735c2c54be6a8abb73b318a
moonshinean <710159940@qq.com>
2021-03-16 17:53:18 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>你好 {{msg}}</div>
+</template> 
+<script lang="ts">
+import {defineComponent, ref} from 'vue'
+export default defineComponent({
+    name: 'home',
+    setup(){
+        const msg = ref<string>('vite');
+
+        return {
+            msg
+        }
+    }
+})
+</script>  
